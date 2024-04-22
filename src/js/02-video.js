0616d9ccfddbac05a77b1d2e82710263987049f9
lancekasturias <lanceasturias@gmail.com>
2024-04-22 15:28:02 +0800

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
 
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Player(iframe);
+
+const timeFunction = data => {
+    let playerSecond = data.seconds;
+    localStorage.setItem("videolayer-cuurrent-time", playerSecond);
+}
+
+player.on('timeupdate', throttle(timeFunction, 1000));
+
+player.setCurrentTime(localStorage.getItem("videolayer-cuurrent-time")).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('form.feedback-form');
+const emailEL = document.querySelector('label [name="email"]');
+const messageEL = document.querySelector('label [name="message"]');
+
+
+const STORAGE_KEY = 'feedback-form-state';
+
+
+function onPageReload(){
+    const saveMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if(saveMessage){
+emailEL.value = saveMessage.email;
+messageEL.value = saveMessage.message;
+
+    }
+}
+
+onPageReload();
+
+function onFormInput(){ 
+    const email = emailEL.value;
+    const message = messageEL.value;
+
+    const formData = {
+        email,
+        message,
+    };
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormSubmit(e){
+e.preventDefault();
+const email = emailEL.value;
+const message = messageEL.value;
+
+if ( email == '' || message == ''){
+    alert('Enter both input parameters');
+    form.reset();
+    return;
+}
+
+form.reset();
+localStorage.removeItem(STORAGE_KEY);
+}
+
+form.addEventListener('submit', onFormSubmit);
